f473e10aa8ff4845dbadacd23b3eb71e7e53a140
CatsHaveFeet <and.surubariu@gmail.com>
2022-08-03 18:23:26 +0300

    mess

---
+++ Basic Data Types Activity/basicDataTypes.js
@@ -0,0 +1,9 @@
+let firstSentence = "Tom was a ";
+let secondSentence = " year old boy.\nThe year is ";
+let thirdSentence =  " and it was ";
+let fourthSentence = "pm on a hot summer's day. ";
+let age = 14;
+let year = 2012;
+let time = 3;
+let items = ["pens", "rubber bands", "paper clips"];
+console.log(firstSentence + age + secondSentence + year + thirdSentence + time + fourthSentence + "\n" + "He has " + items[0] + ", " + items[1] + " and " + items[2] + ".\nBecause of the heat, Tom sets off on his bike to get some ice cream.\nLuckily, just as he heads for the local shop, he hears an ice cream truck approaching.\nHe gets himself a cold popsicle and proceeds to lay on his couch relaxing.\nIt was a nice day. ");
